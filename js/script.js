33dd774b60e691a75089a4bd881d4e66c00ce8af
Michela30 <micheladestefano30@gmail.com>
2023-06-13 17:53:39 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ problema: Calcolo del prezzo del biglietto del treno
 - Creare form
 1) Chiedere all'utente quanti km vuole percorrere come input
 2) Chiedere all'utente l'età come input
-
 3) Creare button che verrà utilizzato per sottoscrivere il form
-
 4) Calcolare prezzo totale del viaggio
     -attenzione, il biglietto è di 0.21€ al km
 5) SE l'utente è minorenne
@@ -32,24 +30,47 @@ function(){
 
         let age = document.getElementById("age").value;
 
+        let name = document.getElementById("name").value;
+
         const priceperKm = 0.21;
         let price = (priceperKm * km);
         let discount = 0;
 
+        if (isNaN(km) || (age == '0')){
+            alert ("Errore");
+        } else {
+            // SE l'utente è minorenne o over
 
-        // SE l'utente è minorenne o over
+                if(age == 'minorenne'){
+                    discount = (price * 0.2);
 
-            if(age == 'minorenne'){
-                discount = (price * 0.2);
+                }else if(age == 'over'){
+                    discount = (price * 0.4);
 
-            }else if(age == 'over'){
-                discount = (price * 0.4);
+                }
+                //qui ci sarà la somma per fare il prezzo
+            
+                price = ((priceperKm * km) - discount);
+                console.log ('price', price, typeof price);
 
+
+            
+            //stampa in pagina
+            document.querySelector('.price-area').innerHTML = '€' + price;
+            document.querySelector('.nameFinal').innerHTML = name;
+
+            document.querySelector('.carrozza').innerHTML = Math.floor(Math.random() * 11);
+            document.querySelector('.code-cp').innerHTML = Math.floor(Math.random() * 99000);
+
+            const payButton = document.getElementById('payment');
+
+            payButton.addEventListener('click',
+            function(){
+                alert ('Congratulazioni! Sarai reindirizzato al pagamento!')
             }
-            //qui ci sarà la somma per fare il prezzo
-           
-            price = ((priceperKm * km) - discount);
-            console.log ('price', price, typeof price)
+            )
+        }
+    }
+    );
 
-}
-);
+
