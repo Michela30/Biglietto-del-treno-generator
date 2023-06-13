4c7849025671241409844c106060c1f0d382007a
Michela30 <micheladestefano30@gmail.com>
2023-06-13 13:57:28 +0200

    inizializzazione progetto

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* 
+problema: Calcolo del prezzo del biglietto del treno
+
+- Creare form
+1) Chiedere all'utente quanti km vuole percorrere come input
+2) Chiedere all'utente l'età come input
+
+3) Creare button che verrà utilizzato per sottoscrivere il form
+
+4) Calcolare prezzo totale del viaggio
+    -attenzione, il biglietto è di 0.21€ al km
+5) SE l'utente è minorenne
+    ALLORA si applica sconto del 20%
+6) SE l'utente è over 65
+    ALLORA si applica sconto del 40%
+7) Indicare prezzo finale in pagina con la somma dei km ed età (con i possibili sconti)
+8) 
+
+*/
